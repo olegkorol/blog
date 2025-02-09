d6d2f0d431c4b8e307686379d655cd622e267e57
Oleg Korol <oleg@olegkorol.de>
2025-02-09 11:35:33 +0400

    improve sitemap.xml slightly

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,29 +1,39 @@
 import { getPosts } from '../posts/get-posts.ts'
 import config from '../config.ts'
+
+// we want to get the most recent modification or publication date accross all posts
+const dates: Date[] = []
+const lastModified = () => {
+  return dates.sort((a, b) => a.getTime() - b.getTime())[dates.length - 1].toISOString()
+}
  
 export async function GET() {
   const allPosts = await getPosts()
   const posts = allPosts
     .map(
-      post => `  <url>
+      post => {
+        const thisPostLastModified = post.frontMatter.updated ? new Date(post.frontMatter.updated) : new Date(post.frontMatter.date)
+        dates.push(thisPostLastModified)
+        return `  <url>
     <loc>${config.siteUrl}${post.route}</loc>
-    <lastmod>${post.frontMatter.updated ? new Date(post.frontMatter.updated).toISOString() : new Date(post.frontMatter.date).toISOString()}</lastmod>
+    <lastmod>${thisPostLastModified.toISOString()}</lastmod>
     <changefreq>yearly</changefreq>
     <priority>0.7</priority>
   </url>`
+      }
     )
     .join('\n')
   const xml = `<?xml version="1.0" encoding="UTF-8" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   <url>
     <loc>${config.siteUrl}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+    <lastmod>${lastModified()}</lastmod>
     <changefreq>yearly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>${config.siteUrl}/posts</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+    <lastmod>${lastModified()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
@@ -32,7 +42,7 @@ ${posts}
  
   return new Response(xml, {
     headers: {
-      'Content-Type': 'application/rss+xml'
+      'Content-Type': 'application/xml'
     }
   })
 }
